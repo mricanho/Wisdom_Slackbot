c9a926077562c667c44db7a6a6bc618c5b3bcdaa
Miguel Ricaño <mricanho@gmail.com>
2020-12-24 09:08:51 -0600

    Add require_relative whenever is possible

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -1,22 +1,24 @@
 require './lib/commands'
-describe GetCommand < SlackRubyBot::Commands::Base do
-  it 'returns a random video from the RSS list' do
-    expect(message: 'I want a video').to respond_with_slack_message
-  end
-
-  it 'returns the last video of the RSS list' do
-    expect(message: "#{SlackRubyBot.config.user} I want an old video").to respond_with_slack_message
-  end
-
-  it 'returns the newest video of the RSS list' do
-    expect(message: "#{SlackRubyBot.config.user} I want a new video").to respond_with_slack_message
-  end
-
-  it 'returns a quote' do
-    expect(message: "#{SlackRubyBot.config.user} I want a quote").to respond_with_slack_message
-  end
-
-  it 'says hello' do
-    expect(message: "#{SlackRubyBot.config.user} say_hello").to respond_with_slack_message
+require_relative 'spec_helper'
+require 'slack-ruby-bot'
+module WisdomBot
+  module Commands
+    describe GetCommand < SlackRubyBot::Commands::Base do
+      it 'returns a random video from the RSS list' do
+        expect(message: "#{SlackRubyBot.config.user} I want a video").to respond_with_slack_message
+      end
+      it 'returns the last video of the RSS list' do
+        expect(message: "#{SlackRubyBot.config.user} I want an old video").to respond_with_slack_message
+      end
+      it 'returns the newest video of the RSS list' do
+        expect(message: "#{SlackRubyBot.config.user} I want a new video").to respond_with_slack_message
+      end
+      it 'returns a quote' do
+        expect(message: "#{SlackRubyBot.config.user} I want a quote").to respond_with_slack_message
+      end
+      it 'says hello' do
+        expect(message: "#{SlackRubyBot.config.user} say_hello").to respond_with_slack_message
+      end
+    end
   end
 end
